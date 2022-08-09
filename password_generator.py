db500399ed84c9850f6aa807975fc46c28b946aa
JerrodxHavig <jhavig99@gmail.com>
2022-08-08 22:08:19 -0700

    created random function

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,26 @@
 import string
 import random
 
-# characters for password generation.
+# Characters for password generation.
+characters = list(string.ascii_letters + string.digits + '!@#$%^&*()')
 
+
+def random_password():
+    # Password length
+    length = int(input('Password length: '))
+
+    # Shuffle characters
+    random.shuffle(characters)
+
+    # Pick random character from the list
+    password = []
+    for i in range(length):
+        password.append(random.choice(characters))
+
+    # Shuffle password
+    random.shuffle(password)
+
+    print(''.join(password))
+
+
+random_password()
